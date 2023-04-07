a5a29255db965d45c7af320939cd96ae3425fbc3
Souhail Benlhachemi <souhailbenlhachemi1999@gmail.com>
2023-04-07 21:46:45 +0000

    fix product card title

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -7,7 +7,7 @@ export default function Category({ category, products, whatsapp }: { category: t
     return(
         <div className='space-y-8' key={category.id}>
             <div className="divider after:bg-gray-300 before:bg-gray-300">
-                <h2 className='text-gray-100 font-bold text-2xl mx-auto text-center py-1 px-3 rounded-tl-2xl rounded-br-2xl bg-gray-900'>{category.name}</h2>
+                <h2 className='text-gray-100 font-bold text-lg mx-auto text-center py-1 px-3 rounded-tl-2xl rounded-br-2xl bg-gray-900'>{category.name}</h2>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 space-y-6 lg:space-y-0">
                 {products.map(product => (
